a070afb6ae8105d5dd5a0a61187ee5a5fbf17f71
19elka <268888913+19elka@users.noreply.github.com>
2026-03-19 17:05:14 +0300

    Скорректировала миграции

--- src/main/resources/db/changelog/changeset/17.03.2026_init-users.sql
+++ src/main/resources/db/changelog/changeset/17.03.2026_init-users.sql
@@ -1,17 +1,19 @@
-INSERT INTO users (username, password, roles, name)
+INSERT INTO users (username, password, name)
 SELECT 'admin',
        '$2a$10$VpWIm6QdfIBdSEn5BF2.POd6duxt3x3qWwPUpjDeaxBaYHJBNfz5K',
-       'ADMIN',
        'Admin User' WHERE NOT EXISTS (SELECT 1 FROM users WHERE username = 'admin');
 
-INSERT INTO users (username, password, roles, name)
+INSERT INTO users (username, password, name)
 SELECT 'user',
        '$2a$10$jtANSJ3.LBpc.Tw539Rov.VGxHEOv0vYBXS9BJWG3aG3UXKr7Fo8q',
-       'USER',
        'Regular User' WHERE NOT EXISTS (SELECT 1 FROM users WHERE username = 'user');
 
 INSERT INTO user_roles (user_id, roles)
-SELECT id, 'ADMIN' FROM users WHERE username = 'admin';
+SELECT id, 'ADMIN'
+FROM users
+WHERE username = 'admin';
 
 INSERT INTO user_roles (user_id, roles)
-SELECT id, 'USER' FROM users WHERE username = 'user';
+SELECT id, 'USER'
+FROM users
+WHERE username = 'user';
